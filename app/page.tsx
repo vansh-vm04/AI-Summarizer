fee14aee1f43987680839da9ba12be0947b43576
Vansh <vanshmorwani1@gmail.com>
2025-08-17 01:35:11 +0530

    frontend page done

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,115 @@
-import Image from "next/image";
+"use client";
+import { useRef, useState } from "react";
+import toast from "react-hot-toast";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const summaryRef = useRef<HTMLTextAreaElement>(null);
+  const [transcript, setTranscript] = useState("");
+  const [prompt, setPrompt] = useState("");
+  const [summary, setSummary] = useState("");
+  const [email, setEmail] = useState("");
+  const [loading,setLoading] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const handleGenerate = async () => {
+    if(!transcript){
+      toast.error("Enter transcript to generate summary");
+      return;
+    }
+    setSummary('')
+    setLoading(true);
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/summarize`,{
+      method:'POST',
+      headers: { "Content-Type": "application/json" },
+      body:JSON.stringify({
+        transcript,
+        prompt:prompt || 'Summarize this'
+      })
+    })
+
+    if(!response.body){
+      toast.error('There was an error, try again');
+      return;
+    }
+
+    const reader = response.body?.getReader();
+    const decoder = new TextDecoder();
+
+    while(true){
+      const {done,value} = await reader?.read();
+      if(done) break;
+      const chunk = decoder.decode(value,{stream:true});
+      setSummary(prev=>prev+chunk);
+    }
+    setLoading(false);
+  };
+
+  const handleShare = async () => {
+
+  };
+
+  return (
+    <div className="flex flex-col items-center h-screen w-full bg-gray-950 text-white px-6 py-4 gap-4">
+      <span className="text-4xl font-bold text-transparent bg-gradient-to-br from-purple-400 to-blue-700 bg-clip-text px-8 py-4 rounded-xl shadow-lg">
+        AI Summarizer
+      </span>
+
+      <div className="w-full max-w-5xl flex flex-col gap-6">
+        
+
+        <textarea
+          onChange={(e) => setTranscript(e.target.value)}
+          value={transcript}
+          placeholder="Enter meeting transcript..."
+          className="w-full h-32 p-4 resize-none hideScrollbar text-gray-900 border border-gray-700 rounded-xl bg-gray-100 text-base focus:ring-2 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:text-white"
+        />
+
+        <div className="flex gap-4 items-center">
+          <input
+            onChange={(e) => setPrompt(e.target.value)}
+            type="text"
+            value={prompt}
+            placeholder="Provide instructions for summary..."
+            className="flex-1 bg-gray-100 border border-gray-700 text-gray-900 text-sm rounded-xl p-3 focus:ring-2 focus:ring-blue-500 dark:bg-gray-800 dark:text-white"
+          />
+          <button
+          disabled={loading}
+            onClick={handleGenerate}
+            type="button"
+            className="text-white bg-gradient-to-r from-purple-600 to-blue-500 hover:from-purple-500 hover:to-blue-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-xl text-sm px-6 py-3"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            Generate Summary
+          </button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+
+        
+        <textarea
+        ref = {summaryRef}
+          onChange={(e) => setSummary(e.target.value)}
+          value={summary}
+          readOnly={loading}
+          placeholder="Your generated summary will appear here..."
+          className="w-full hideScrollbar resize-none overflow-y-auto h-[40vh] p-4 text-white bg-gray-800 rounded-xl outline-none focus:ring-2 focus:ring-blue-500"
+        />
+        <div className="flex gap-4 items-center">
+          
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            value={email}
+            placeholder="Recipient email..."
+            className="flex-1 bg-gray-100 border border-gray-700 text-gray-900 text-sm rounded-xl p-3 focus:ring-2 focus:ring-blue-500 dark:bg-gray-800 dark:text-white"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <button
+          disabled={loading}
+            onClick={handleShare}
+            type="button"
+            className="text-white disabled:bg-green-900 bg-green-600 hover:bg-green-500 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-xl text-sm px-6 py-3"
+          >
+            Share Summary
+          </button>
+          <button onClick={()=>setSummary('')} className="text-white bg-red-600 hover:bg-red-500 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-xl text-sm px-6 py-3">Clear Summary</button>
+        </div>
+      </div>
     </div>
   );
 }
